c140bfc17d3b66a069222f693af48dc88c0749f5
Blake Mealey <blakemealey@gmail.com>
2020-06-02 17:12:38 -0600

    Added initial extension

--- README.md
+++ README.md
@@ -1,65 +1,27 @@
-# generate-wt-scheme README
+# Generate Windows Terminal Scheme
 
-This is the README for your extension "generate-wt-scheme". After writing up a brief description, we recommend including the following sections.
+Provides a command to generate a Windows Terminal color scheme from your currently active
+VSCode color theme, to enable easily matching WT with VSCode for beautiful dev
+environments.
 
-## Features
+![Result](./images/result.png)
 
-Describe specific features of your extension including screenshots of your extension in action. Image paths are relative to this README file.
+_Using the [Night Owl](https://marketplace.visualstudio.com/items?itemName=sdras.night-owl) theme_
 
-For example if there is an image subfolder under your extension project workspace:
+## Usage
 
-\!\[feature X\]\(images/feature-x.png\)
+First, run the `Generate Windows Terminal Scheme` command:
 
-> Tip: Many popular extensions utilize animations. This is an excellent way to show off your extension! We recommend short, focused animations that are easy to follow.
+![Command](./images/command.png)
 
-## Requirements
+You will be prompted to enter the theme name because unfortunately at this time there is
+no VSCode API to get the name of the current active theme (if you know of one, please let
+me know!).
 
-If you have any requirements or dependencies, add a section describing those and how to install and configure them.
+Once it completes, the result will be automatically copied to your clipboard.
 
-## Extension Settings
+Now open your WT settings file (`Ctrl+,` in WT by default), and paste the JSON into the `schemes` array.
 
-Include if your extension adds any VS Code settings through the `contributes.configuration` extension point.
-
-For example:
-
-This extension contributes the following settings:
-
-* `myExtension.enable`: enable/disable this extension
-* `myExtension.thing`: set to `blah` to do something
-
-## Known Issues
-
-Calling out known issues can help limit users opening duplicate issues against your extension.
-
-## Release Notes
-
-Users appreciate release notes as you update your extension.
-
-### 1.0.0
-
-Initial release of ...
-
-### 1.0.1
-
-Fixed issue #.
-
-### 1.1.0
-
-Added features X, Y, and Z.
-
------------------------------------------------------------------------------------------------------------
-
-## Working with Markdown
-
-**Note:** You can author your README using Visual Studio Code.  Here are some useful editor keyboard shortcuts:
-
-* Split the editor (`Cmd+\` on macOS or `Ctrl+\` on Windows and Linux)
-* Toggle preview (`Shift+CMD+V` on macOS or `Shift+Ctrl+V` on Windows and Linux)
-* Press `Ctrl+Space` (Windows, Linux) or `Cmd+Space` (macOS) to see a list of Markdown snippets
-
-### For more information
-
-* [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
-* [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
-
-**Enjoy!**
+See the
+[docs](https://docs.microsoft.com/en-us/windows/terminal/customize-settings/profile-settings)
+for more how to use WT color schemes.
--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,100 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import { camelCase } from "change-case";
+import { parse as parseJson } from "json5";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+function convertThemeToScheme(theme: any, name: string) {
+  const colors = theme.colors as Record<string, string>;
+
+  const ansiPrefix = "ansi";
+
+  const replacements = Object.entries({
+    magenta: "purple",
+    brightMagenta: "brightPurple",
+  });
+
+  const scheme = {
+    name: name,
+    background: colors["editor.background"],
+    foreground: colors["editor.foreground"],
+    cursorColor: colors["editorCursor.foreground"],
+    ...Object.fromEntries(
+      Object.entries(colors)
+        .filter(([key]) => key.startsWith("terminal."))
+        .map(([key, value]) => {
+          // remove key prefixes
+          key = key.split(".")[1];
+          if (key.startsWith(ansiPrefix)) {
+            key = camelCase(key.substring(ansiPrefix.length));
+          }
+
+          // replace keys that have different names
+          const replacement = replacements.find(([k]) => k === key);
+          if (replacement) {
+            key = replacement[1];
+          }
+
+          // chop off alpha channel from values
+          if (value.match(/#\w{8}/)) {
+            value = value.substr(0, 7);
+          }
+
+          return [key, value];
+        })
+    ),
+  };
+
+  return scheme;
+}
+
 export function activate(context: vscode.ExtensionContext) {
+  const disposable = vscode.commands.registerCommand(
+    "extension.generate-wt-scheme.generate-scheme",
+    async () => {
+      const themeName = await vscode.window.showInputBox({
+        placeHolder: "Theme Name",
+      });
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "generate-wt-scheme" is now active!');
+      if (!themeName) {
+        return;
+      }
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+      await vscode.commands.executeCommand(
+        "workbench.action.generateColorTheme"
+      );
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
+      const editor = vscode.window.activeTextEditor;
+      if (editor) {
+        // Get and convert the theme
+        const theme = parseJson(editor.document.getText());
+        const scheme = convertThemeToScheme(theme, themeName);
+        const schemeText = JSON.stringify(scheme, null, 2);
 
-	context.subscriptions.push(disposable);
+        // Write back to the editor
+        await editor.edit((editBuilder) => {
+          const firstLine = editor.document.lineAt(0);
+          const lastLine = editor.document.lineAt(
+            editor.document.lineCount - 1
+          );
+          const range = new vscode.Range(
+            firstLine.range.start,
+            lastLine.range.end
+          );
+
+          editBuilder.replace(range, schemeText);
+        });
+
+        // Write to the clipboard
+        await vscode.env.clipboard.writeText(schemeText);
+
+        // Show a message to say the conversion was successful
+        await vscode.window.showInformationMessage(
+          "The scheme has been generated and copied to your clipboard!"
+        );
+      }
+    }
+  );
+
+  context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
